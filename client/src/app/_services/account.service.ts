65028c3ac830316dc0bebeb4373da06cb9db7fcf
katsarosz-admin <katsarosz@unisystems.gr>
2024-10-25 21:13:42 +0300

    initial

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, model, signal } from '@angular/core';
+import { User } from '../_models/user';
+import { map } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  private http = inject(HttpClient);
+  baseUrl = environment.apiUrl;
+  currentUser = signal<User | null>(null);
+
+  login(model:any){
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map(user => {
+        if (user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+      })
+    );
+  }
+
+  register(model:any){
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if (user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+        
+        return user;
+      })
+    );
+  }
+
+  logout()
+  {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
+  }
+}
---
+++ client/src/app/app.config.ts
@@ -0,0 +1,20 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+import { errorInterceptor } from './_interceptors/error.interceptor';
+import { jwtInterceptor } from './_interceptors/jwt.interceptor';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([errorInterceptor, jwtInterceptor])),
+    provideAnimations(),    
+    provideToastr({
+      positionClass: 'toast-bottom-right'
+    })
+  ]
+};
